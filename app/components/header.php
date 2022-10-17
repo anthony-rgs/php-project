31768e40c76ba26a315f92476b2b1ccc37f692ef
Anthony Ringressi <ringressi.anthony@gmail.com>
2022-10-17 02:00:11 +0200

    feat: add favicon

--- app/components/header.php
+++ app/components/header.php
@@ -3,5 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <title>Réseau social</title>
+  <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
   <link href="../public/css/global.css" rel="stylesheet">
 
